3d6f3ec0b11850f5c510f492b1b24b81f0758540
Gevorg Petrosyan <armen.kharatyuan@menugroup.co.uk>
2025-07-25 09:03:17 +0400

    using useOptimistic hook for like button and revalidating prerendered pages in production

--- actions/posts.js
+++ actions/posts.js
@@ -1,51 +1,58 @@
 "use server";
 
 import { uploadImage } from "@/lib/cloudinary";
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
- export async function createPost(prevState, formData) {
-   const title = formData.get("title");
-   const image = formData.get("image");
-   const content = formData.get("content");
+export async function createPost(prevState, formData) {
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
 
-   const errors = [];
-   if (!title || title.trim() === "") {
-     errors.push("Title is required.");
-   }
+  const errors = [];
+  if (!title || title.trim() === "") {
+    errors.push("Title is required.");
+  }
 
-   if (!content || content.trim() === "") {
-     errors.push("Content is required.");
-   }
+  if (!content || content.trim() === "") {
+    errors.push("Content is required.");
+  }
 
-   console.log("image:", image);
+  console.log("image:", image);
 
-   if (!image || image.size === 0) {
-     errors.push("Image is required.");
-   }
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
 
-   if (errors.length > 0) {
-     return {
-       errors,
-     };
-   }
+  if (errors.length > 0) {
+    return {
+      errors,
+    };
+  }
 
-   let imageUrl;
+  let imageUrl;
 
-   try {
-      imageUrl = await uploadImage(image);
-   } catch (error) {
-     throw new Error("Image upload failed, post was not created .Please try again later." );
-   }
-   
-   
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Image upload failed, post was not created .Please try again later."
+    );
+  }
 
-   storePost({
-     imageUrl,
-     title,
-     content,
-     userId: 1, // Assuming a static user ID for this example
-   });
+  storePost({
+    imageUrl,
+    title,
+    content,
+    userId: 1, // Assuming a static user ID for this example
+  });
+  
+  revalidatePath("/", "layout");
+  redirect("/feed");
+}
 
-   redirect("/feed");
- }
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
+}
